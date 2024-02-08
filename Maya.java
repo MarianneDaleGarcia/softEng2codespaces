e6af4cbe51f7e3961daf3ec47e95313fef1012ad
MarianneDaleGarcia <143275928+MarianneDaleGarcia@users.noreply.github.com>
2024-02-08 00:17:36 +0000

    firstcommit

---
+++ Maya.java
@@ -0,0 +1,10 @@
+public class Maya implements PaymentType{
+    public String calculatePayment(){
+
+    String output= "";
+    output += "Payment is Maya\n";
+    output += "You will receive additional 1% interest on Savings";
+
+    return output;
+    }
+}
